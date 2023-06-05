55c320c3d5b40fdba7ebbb92f6861d0badd6acdb
Jordan Mruczynski <joromruc@gmail.com>
2023-06-05 15:00:38 +0200

    - configured communication between docker containers - added 'docker' spring profile for building images - fixed bug (tag:#01a) apigateway 'service unavailable' (added http bean, and enabled eureka discovery) - configured build-docker-image profile

--- apigw/src/main/java/com/jordanmruczynski/apigw/ApiGWApplication.java
+++ apigw/src/main/java/com/jordanmruczynski/apigw/ApiGWApplication.java
@@ -1,13 +1,22 @@
 package com.jordanmruczynski.apigw;
 
+import io.netty.resolver.DefaultAddressResolverGroup;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import reactor.netty.http.client.HttpClient;
 
-@SpringBootApplication
+
 @EnableEurekaClient
+@SpringBootApplication
 public class ApiGWApplication {
     public static void main(String[] args) {
         SpringApplication.run(ApiGWApplication.class, args);
+    }
+
+    @Bean
+    public HttpClient httpClient() {
+        return HttpClient.create().resolver(DefaultAddressResolverGroup.INSTANCE);
     }
 }
